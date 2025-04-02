4238c1452bf69ad6efc5b949c15105794f6ec3ab
2368871983 <2368871983@qq.com>
2025-04-02 21:07:56 +0800

    添加请求和文件上传拦截器

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,4 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import '@/utils/http'
+import { onMounted } from 'vue'
+onMounted(() => {
+  const getdata = () => {
+    uni.request({
+      url: '/home/banner',
+      method: 'GET',
+    })
+  }
+  getdata()
+})
+</script>
 
 <template>
   <uni-card
@@ -8,6 +20,7 @@
     thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
     ><text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
   </uni-card>
+  <button>2131</button>
 </template>
 
 <style lang="scss"></style>
