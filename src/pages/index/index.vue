b43c90f24c0d1cccf0bdede65d1579a5a7f725ba
2368871983 <2368871983@qq.com>
2025-04-03 21:20:14 +0800

    首页-自定义导航栏

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,26 +1,10 @@
 <script setup lang="ts">
-import '@/utils/http'
-import { onMounted } from 'vue'
-onMounted(() => {
-  const getdata = () => {
-    uni.request({
-      url: '/home/banner',
-      method: 'GET',
-    })
-  }
-  getdata()
-})
+import CustomNavbar from '@/pages/index/components/CustomNavbar.vue'
+import '@/utils/request'
 </script>
 
 <template>
-  <uni-card
-    title="基础卡片"
-    sub-title="副标题"
-    extra="额外信息"
-    thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
-    ><text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
-  </uni-card>
-  <button>2131</button>
+  <CustomNavbar></CustomNavbar>
 </template>
 
 <style lang="scss"></style>
---
+++ src/utils/request.ts
@@ -0,0 +1,59 @@
+import { useMemberStore } from '@/stores/index'
+
+const baseURL = 'https://pcapi-xiaotuxian-front-devtest.itheima.net'
+
+const requestInterceptor = {
+  invoke(options: UniApp.RequestOptions) {
+    if (!options.url.startsWith('http')) {
+      options.url = baseURL + options.url
+    }
+    options.timeout = 10000
+    options.header = {
+      ...options.header,
+      'source-client': 'miniapp',
+    }
+    const memberStore = useMemberStore()
+    const token = memberStore.profile?.token
+    if (token) {
+      options.header.Authorization = token
+    }
+  },
+}
+interface Data<T> {
+  code: string
+  msg: string
+  result: T
+}
+export const request = <T>(options: UniApp.RequestOptions) => {
+  return new Promise<Data<T>>((resolve, reject) => {
+    uni.request({
+      ...options,
+      success(res) {
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+          resolve(res.data as Data<T>)
+        } else if (res.statusCode === 401) {
+          // 401错误 清理用户信息，跳转到登录页
+          const memberStore = useMemberStore()
+          memberStore.clearProfile()
+          uni.navigateTo({ url: '/pages/login/login' })
+          reject(res)
+        } else {
+          // 其他错误 按业务需求处理
+          uni.showToast({
+            icon: 'none',
+            title: (res.data as Data<T>).msg || '请求错误',
+          })
+          reject(res)
+        }
+      },
+      fail(err) {
+        uni.showToast({
+          icon: 'none',
+          title: '网络错误，换个网络试试',
+        })
+      },
+    })
+  })
+}
+uni.addInterceptor('request', requestInterceptor)
+uni.addInterceptor('uploadFile', requestInterceptor)
