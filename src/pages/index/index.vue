31068f8b82baf1a1ab86e5480e386ac653208338
2368871983 <2368871983@qq.com>
2025-04-05 20:25:02 +0800

    前台分类

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,26 +1,38 @@
 <script setup lang="ts">
 import CustomNavbar from '@/pages/index/components/CustomNavbar.vue'
-import { getHomeBannerAPI } from '@/services/home'
+import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 import '@/utils/request'
-import type { BannerItem } from '@/types/home'
+import type { BannerItem, CategoryItem } from '@/types/home'
+import CategoryPanel from './components/CategoryPanel.vue'
 
+// 获取首页轮播图数据
 const BannerList = ref<BannerItem[]>([])
-
 const getHomeBannerData = async () => {
   const res = await getHomeBannerAPI()
   BannerList.value = res.result
 }
 
+// 获取首页分类数据
+const CategoryList = ref<CategoryItem[]>([])
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+  console.log(res)
+  CategoryList.value = res.result
+}
+
+// 页面加载
 onLoad(() => {
   getHomeBannerData()
+  getHomeCategoryData()
 })
 </script>
 
 <template>
   <CustomNavbar></CustomNavbar>
   <XtxSwiper :bannerList="BannerList"></XtxSwiper>
+  <CategoryPanel :categoryList="CategoryList"></CategoryPanel>
 </template>
 
 <style lang="scss"></style>
--- src/services/home.ts
+++ src/services/home.ts
@@ -1,6 +1,7 @@
-import type { BannerItem } from '@/types/home.d'
+import type { BannerItem, CategoryItem } from '@/types/home.d'
 import { request } from '@/utils/request'
 
+// 获取首页轮播图
 export const getHomeBannerAPI = () => {
   return request<BannerItem[]>({
     url: '/home/banner',
@@ -8,5 +9,13 @@ export const getHomeBannerAPI = () => {
     data: {
       distributionSite: 1,
     },
+  })
+}
+
+// 获取首页分类
+export const getHomeCategoryAPI = () => {
+  return request<CategoryItem[]>({
+    url: '/home/category/mutli',
+    method: 'GET',
   })
 }
